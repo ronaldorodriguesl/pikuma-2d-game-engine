e73334d83ef5b41e159d1571e0de0331a2ba8062
Ronaldo Rodrigues <ronaldorodriguesl@outlook.com>
2026-07-25 21:58:53 -0300

    Keyboard Control System

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -14,6 +14,8 @@
 #include "../Components/SpriteComponent.h"
 #include "../Components/AnimationComponent.h"
 #include "../Components/BoxColliderComponent.h"
+#include "../Components/KeyboardControlledComponent.h"
+
 #include "../Systems/MovementSystem.h"
 #include "../Systems/RenderSystem.h"
 #include "../Systems/AnimationSystem.h"
@@ -105,7 +107,7 @@ void Game::LoadLevel(int level)
     assetStore->AddTexture(renderer, "tank-image", "./assets/images/tank-panther-right.png");
     assetStore->AddTexture(renderer, "truck-image", "./assets/images/truck-ford-right.png");
     assetStore->AddTexture(renderer, "jungle", "./assets/tilemaps/jungle.png");
-    assetStore->AddTexture(renderer, "chopper-image", "./assets/images/chopper.png");
+    assetStore->AddTexture(renderer, "chopper-image", "./assets/images/chopper-spritesheet.png");
     assetStore->AddTexture(renderer, "radar-image", "./assets/images/radar.png");
 
     int tileSize = 32;
@@ -144,6 +146,11 @@ void Game::LoadLevel(int level)
     chopper.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
     chopper.AddComponent<SpriteComponent>("chopper-image", 32, 32, 1);
     chopper.AddComponent<AnimationComponent>(2, 15, true);
+    chopper.AddComponent<KeyboardControlledComponent>(
+        glm::vec2(0.0, -20.0),
+        glm::vec2(20.0, 0.0),
+        glm::vec2(0.0, 20.0),
+        glm::vec2(-20.0, 0.0));
 
     Entity radar = registry->CreateEntity();
     radar.AddComponent<TransformComponent>(glm::vec2(windowWidth - 74, 10.0), glm::vec2(1.0, 1.0), 0.0);
--- src/Systems/KeyboardControlSystem.h
+++ src/Systems/KeyboardControlSystem.h
@@ -6,26 +6,58 @@
 #include "../Events/KeyPressedEvent.h"
 #include "../Components/RigidBodyComponent.h"
 #include "../Components/SpriteComponent.h"
+#include "../Components/KeyboardControlledComponent.h"
+class KeyboardControlSystem : public System
+{
+public:
+    KeyboardControlSystem()
+    {
+        RequireComponent<KeyboardControlledComponent>();
+        RequireComponent<SpriteComponent>();
+        RequireComponent<RigidBodyComponent>();
+    }
 
-class KeyboardControlSystem: public System {
-    public:
-        KeyboardControlSystem() {
-        
-        }
+    void SubscribeToEvents(std::unique_ptr<EventBus> &eventBus)
+    {
+        eventBus->SubscribeToEvent<KeyPressedEvent>(this, &KeyboardControlSystem::OnKeyPressed);
+    }
 
-        void SubscribeToEvents(std::unique_ptr<EventBus>& eventBus) {
-            eventBus->SubscribeToEvent<KeyPressedEvent>(this, &KeyboardControlSystem::OnKeyPressed);
-        }
+    void OnKeyPressed(KeyPressedEvent &event)
+    {
+        for (auto entity : GetSystemEntities())
+        {
+            const auto keyboardControl = entity.GetComponent<KeyboardControlledComponent>();
+            auto &sprite = entity.GetComponent<SpriteComponent>();
+            auto &rigidBody = entity.GetComponent<RigidBodyComponent>();
 
-        void OnKeyPressed(KeyPressedEvent& event) {
-            std::string keyCode = std::to_string(event.symbol);
-            std::string keySymbol(1, event.symbol);
-            Logger::Log("Key pressed event emitted: [" + keyCode + "] " + keySymbol);
-        }
+            switch (event.symbol)
+            {
+            case SDLK_UP:
+                rigidBody.velocity = keyboardControl.upVelocity;
+                sprite.srcRect.y = sprite.height * 0;
+                break;
+            case SDLK_RIGHT:
+                rigidBody.velocity = keyboardControl.rightVelocity;
+                sprite.srcRect.y = sprite.height * 1;
+                break;
+            case SDLK_DOWN:
+                rigidBody.velocity = keyboardControl.downVelocity;
+                sprite.srcRect.y = sprite.height * 2;
+                break;
+            case SDLK_LEFT:
+                rigidBody.velocity = keyboardControl.leftVelocity;
+                sprite.srcRect.y = sprite.height * 3;
+                break;
 
-        void Update() {
-                
+            default:
+                break;
+            }
         }
+    }
+
+    void Update()
+    {
+    }
 };
 
 #endif
